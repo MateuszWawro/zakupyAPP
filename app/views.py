0ed38cf0d37e9a63c1144cbd44f70c0c9af7338e
MateuszWawro <mateusz.wawro4@gmail.com>
2022-03-31 20:18:21 +0200

    widok formularza dodawnia produktu

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
-from flask import render_template, flash, url_for, request
+from flask import render_template, flash, url_for, request, redirect
+from sqlalchemy.exc import DBAPIError
 from .forms import AddProduct
 from .models import DbModel
 from app import app, db
@@ -7,3 +8,26 @@ from app import app, db
 @app.route('/homepage')
 def home_page():
     return render_template ("base.html", title="")
+
+
+@app.route('/addproduct')
+def view_add_product():
+    form = AddProduct()
+    product_list = DbModel.query.all()
+    if form.validate_on_submit():
+        try:
+            q = DbModel(product=form.product.data, producer=form.producer.data, price=form.price.data)
+            db.session.add(q)
+        except DBAPIError as e:
+            db.session.rollback()
+            flash(e.detail)
+        else:
+            db.session.commit()
+            product_list = DbModel.query.all()
+            return redirect(url_for('view_add_product'))
+    else:
+        flash(form.errors)
+        print(form.errors)
+    return render_template("productadd.html", title="")
+
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from app import app
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', port=1111, debug=True)
+    app.run(debug=True ,port=8080,use_reloader=False)
