4f2d3966c6dca71eddcd998cea4e748958eff083
mwawro <mateusz.wawro4@gmail.com>
2022-04-01 12:02:52 +0200

    pip install flask_login class UserModel dodane

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from . import db
-
+from sqlalchemy import String
+from flask_login import UserMixin
 
 class DbModel(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -8,3 +9,13 @@ class DbModel(db.Model):
     price = db.Column(db.Numeric(3))
     ilosc = db.Column(db.Integer())
 
+
+
+class UserModel(db.Model, UserMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(15))
+    surname = db.Column(db.String(15))
+    login = db.Column(db.String(15),  unique=True)
+    email = db.Column(db.String(120), unique=True )
+    password = db.Column(db.String(32), nullable=True)
+    role = db.Column(db.Boolean())
